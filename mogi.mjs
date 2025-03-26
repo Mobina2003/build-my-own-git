5085c1f3b6268a3cfbce86b2a3f001acff1cf9e0
Mobina2003 <habebemobina@gmail.com>
2025-03-26 23:09:40 +0400

    add method1

--- mogi.mjs
+++ mogi.mjs
@@ -1,12 +1,13 @@
 import path from 'path';
 import fs from 'fs/promises';
+import crypto from 'crypto';
 class mogi
 {
     constructor(repoPath='.')
     {
         this.repoPath = path.join(repoPath,'.mogi');
-        this.objectspath = path.join(this.repoPath,'objects'); //.mogi/objects
-        this.headPath = path.join(this.repoPath, 'HEAD'); //.mogi/HEAD
+        this.objectspath = path.join(this.repoPath,'objects'); //.mogi/objects( store repository objects )
+        this.headPath = path.join(this.repoPath, 'HEAD'); //.mogi/HEAD( track the current branch or commit)
         //staging
         this.indexpath = path.join(this.repoPath , 'index'); //.mogi/index
         this.init();
@@ -23,7 +24,26 @@ class mogi
         {
             console.log('already initialized .mogi folder');
         }
-        
-        
+   }
+   hashObject(content)
+   {
+        return crypto.createHash('sha1').update(content , 'utf-8').digest('hex');
+   } 
+   async add(filesToBeAdded)
+   {
+        const fileData = await fs.readFile(filesToBeAdded, {encoding:'utf-8'});
+        const fileHash = this.hashObject(fileData);
+        console.log(fileHash);
+        const newFileObjectPath = path.join(this.objectspath, fileHash);
+        await fs.writeFile(newFileObjectPath, fileData);
+        //add the file to the staging area
+        console.log('added  ${filesToBeAdded}');
+
+    }
+
+    async updateTheStagingArea(filePath,fileHash)
+    {
+        const index = JSON.parse(await fs.readFile(this.indexpath,{encoding: 'utf-8'}));
+        index.push({path :filePath , hash:fileHash}); //add file to the index
     }
 }
